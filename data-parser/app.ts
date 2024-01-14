d05057ba75ff85db894e2818b23c890e9a3e27d1
Konkata Vijay Kumar (Trianz) <vijaykumar.konkata@trianz.com>
2024-01-14 15:55:46 +0530

    JSON file processing and insert into table

--- data-parser/app.ts
+++ data-parser/app.ts
@@ -4,7 +4,8 @@ import path from "path";
 import os from 'os';
 import downloadFileFromS3 from "./getFileFromS3";
 import { getDetails } from "./database";
-
+import yaml from "js-yaml";
+import fs from "fs";
 
 const s3 = new S3Client({ region: process.env.AWS_REGION || 'us-east-1' });
 
@@ -15,7 +16,7 @@ function extractS3Info(event: S3CreateEvent) {
   return { bucket, key };
 }
 
-const supportedFormats = ['csv', 'xml', 'yaml'];
+const supportedFormats = ['csv', 'xml', 'yaml', 'json'];
 
 export const lambdaHandler = async (event: S3CreateEvent, context: Context): Promise<string> => {
     let message: string = 'Lambda invoked successfully with Postgres';
@@ -33,23 +34,56 @@ export const lambdaHandler = async (event: S3CreateEvent, context: Context): Pro
           if (!supportedFormats.includes(extensionWithoutDot)) {
             throw new Error(`unsupported file type ${extension}`);
         }
-       const data = await downloadFileFromS3(s3Info.bucket, s3Info.key, tempFile);
-       console.log('Data received : \n-' + data);
 
-       let employees: { name: string, gender:string, designation: string, email:string,  date_of_join: string }[] = [
-        {  "name": "VijayK2", "gender": "Male", "designation":"Tech Lead2", "email":"vk12@gmail.com","date_of_join":"2021-07-05"},
-        {  "name": "Subha2", "gender": "Female", "designation":"Tech Lead2", "email":"subha2@gmail.com","date_of_join":"2021-02-04"}        
-        ];
+       let dataInString = await downloadFileFromS3(s3Info.bucket, s3Info.key);
+      //  console.log('DowndledData in dataInString : \n-' + dataInString);
+
+       let dataRemovedRegex = dataInString.replace(/\r?\n|\r/g, '');
+                    
+      // JSON File processing
+      if(extensionWithoutDot == 'json') {
+        // console.log(`File conversion from ${extensionWithoutDot} to JSON`);
+                 
+        const employeesObj  = JSON.parse(dataRemovedRegex); 
+        console.log(employeesObj);
+                        
+        var newEmpArray : any[] = [];
+        for (var key in employeesObj) {
+          // skip loop if the property is from prototype
+          if (!employeesObj.hasOwnProperty(key)) continue;          
+                            
+          var obj = employeesObj[key];
           
-            let insertQuery = `INSERT INTO public.employees( name, gender, designation, email, date_of_join) VALUES ` +
-           employees.map( 
-            (emp) =>`('${emp.name}', '${emp.gender}', '${emp.designation}', '${emp.email}', '${emp.date_of_join}')`)
-            .join(', ')
+          for (var prop of obj) {              
+              const dynamicObj: {name: string, gender: string, designation: string, email: string, date_of_join: string} = {
+                name : prop.name,
+                gender : prop.gender,
+                designation : prop.designation,
+                email : prop.email,
+                date_of_join : prop.date_of_join
+              }
+              newEmpArray.push(dynamicObj);              
+          }
+      }
+      console.log(newEmpArray);
+          
+         let insertQuery = `INSERT INTO public.employees( name, gender, designation, email, date_of_join) VALUES ` +
+         newEmpArray.map( 
+              (emp) =>`('${emp.name}', '${emp.gender}', '${emp.designation}', '${emp.email}', '${emp.date_of_join}')`)
+              .join(', ')
 
-            console.log("Before replace :"+insertQuery);
-                               
-      const { rows } = await getDetails(insertQuery);
-        console.log(JSON.stringify(rows))
+              console.log(`json conversion Json : ${insertQuery}`)
+
+               const { rows } = await getDetails(insertQuery);
+               console.log(JSON.stringify(rows))
+      }
+      
+      // YAML File processing
+      if(extensionWithoutDot == 'yaml') {
+        console.log(`File conversion from ${extensionWithoutDot} to YAML`);
+        const employees = yaml.load(fs.readFileSync(dataInString, {encoding: 'utf-8'}));
+        console.log('DowndledData in jsonData :'+employees);
+      }
 
     } catch (error) {
       console.log("Error occured in Lammda handler :"+ error)
--- data-parser/getFileFromS3.ts
+++ data-parser/getFileFromS3.ts
@@ -1,15 +1,13 @@
 import { S3Client, GetObjectCommandOutput, GetObjectCommand } from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
 
-const s3 = new S3Client({ region: process.env.AWS_REGION || 'us-east-1' });
+const s3 = new S3Client({ region: process.env.AWS_REGION || 'ap-south-1' });
 
 export const asStream = (response: GetObjectCommandOutput) => {
-    console.log('asBuffer asStream...')
     return response.Body as Readable;
   };
   
-  export const asBuffer = async (response: GetObjectCommandOutput) => {
-    console.log('asBuffer start...')
+  export const asBuffer = async (response: GetObjectCommandOutput) => {    
     const stream = asStream(response);
     const chunks: Buffer[] = [];
     return new Promise<Buffer>((resolve, reject) => {
@@ -19,18 +17,17 @@ export const asStream = (response: GetObjectCommandOutput) => {
     });
   };
   
-  export const getDataFromFile = async (response: GetObjectCommandOutput) => {
-    console.log('getDataFromFile start...')
+  export const getDataFromFile = async (response: GetObjectCommandOutput) => {    
     const buffer = await asBuffer(response);
     return buffer.toString();
   };
 
-  async function downloadFileFromS3(bucket: string, fileKey: string, filePath: string) {
-    console.log('Downloading started...', bucket, fileKey, filePath);
+  async function downloadFileFromS3(bucket: string, fileKey: string) {
+    console.log('Downloading started...', bucket, fileKey);
   
     const data = await s3.send(new GetObjectCommand({ Bucket: bucket, Key: fileKey }));
-     const stringData = await getDataFromFile(data)     
-    
+    const stringData = await getDataFromFile(data)     
+  
      return stringData;
   }
 
